04b9e05daac27f5d21d702d28166749180b31dbf
Mika Kobayashi <mika-k@users.noreply.github.com>
2015-01-18 19:29:09 -0500

    Add Japanese translation

---
+++ translations/lxqt-openssh-askpass_ja.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>OpenSSHの認証パスフレーズの要求</translation>
+    </message>
+    <message>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>SSHのパスフレーズを入力してください:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>unknown request</source>
+        <translation>不明な要求</translation>
+    </message>
+</context>
+</TS>
