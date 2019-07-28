4e1914ba12fb4d47ef826438c13c7ecbfebc7eed
parknert <mikael@parknert.se>
2019-07-28 14:20:35 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: LXQt/LXQt OpenSSH Askpass
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-openssh-askpass/sv/

--- translations/lxqt-openssh-askpass_sv.ts
+++ translations/lxqt-openssh-askpass_sv.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../src/mainwindow.ui" line="14"/>
         <source>OpenSSH Authentication Passphrase request</source>
-        <translation type="unfinished"></translation>
+        <translation>OpenSSH Autentisering lösenord</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="20"/>
         <source>Enter your SSH passphrase for request:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ange ditt SSH-lösenord för begäran:</translation>
     </message>
 </context>
 <context>
@@ -19,7 +19,7 @@
     <message>
         <location filename="../src/main.cpp" line="52"/>
         <source>unknown request</source>
-        <translation type="unfinished"></translation>
+        <translation>okänd begäran</translation>
     </message>
 </context>
 </TS>
