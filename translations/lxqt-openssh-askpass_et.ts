5209b8df035d0b0e6b6a000b64ad4873ebd27347
Priit Jõerüüt <lxqtwlate@joeruut.com>
2021-04-29 20:07:15 +0200

    Weblate commits
    Co-authored-by: Priit Jõerüüt <lxqtwlate@joeruut.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-openssh-askpass/et/
    Translation: LXQt Configuration/lxqt-openssh-askpass

---
+++ translations/lxqt-openssh-askpass_et.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="et">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>OpenSSH autentimise salafraasi päring</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="20"/>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>Päringu vastuseks sisesta oma SSH salafraas:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="52"/>
+        <source>unknown request</source>
+        <translation>tundmatu päring</translation>
+    </message>
+</context>
+</TS>
