0127ef467952fd1887db97172f2745f06643cd53
dimuaconheo <dimuaconheo@gmail.com>
2021-05-16 17:07:25 +0200

    Weblate commits
    Co-authored-by: dimuaconheo <dimuaconheo@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-openssh-askpass/vi/
    Translation: LXQt Configuration/lxqt-openssh-askpass

---
+++ translations/lxqt-openssh-askpass_vi.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>Yêu cầu Mật ngữ Xác thực OpenSSH</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="20"/>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>Nhập mật ngữ SSH của bạn cho yêu cầu:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="52"/>
+        <source>unknown request</source>
+        <translation>yêu cầu không rõ</translation>
+    </message>
+</context>
+</TS>
