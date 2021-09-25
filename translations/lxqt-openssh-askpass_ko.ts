2f64920d44bd7f6c580e843ecb8888e467afd185
이정희 <daemul72@gmail.com>
2021-09-25 20:08:09 +0200

    Weblate commits
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-openssh-askpass/ko/
    Translation: LXQt Configuration/lxqt-openssh-askpass

---
+++ translations/lxqt-openssh-askpass_ko.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>OpenSSH 인증 열쇠글 요청</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="20"/>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>요청에 대한 SSH 열쇠글 입력:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="52"/>
+        <source>unknown request</source>
+        <translation>알 수 없는 요청</translation>
+    </message>
+</context>
+</TS>
