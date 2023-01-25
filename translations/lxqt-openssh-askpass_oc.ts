fbbd4cfb687f3973287b0b38d8afa7f7a4088e92
Quentin PAGÈS <quentinantonin@free.fr>
2023-01-25 07:59:52 +0100

    Weblate commits
    Co-authored-by: Quentin PAGÈS <quentinantonin@free.fr>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-openssh-askpass/oc/
    Translation: LXQt Configuration/lxqt-openssh-askpass

---
+++ translations/lxqt-openssh-askpass_oc.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="oc">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>Senhal de l’autentificacion OpenSSH de la requèsta</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="20"/>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>Picatz lo senhal SSH per la requèsta :</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="52"/>
+        <source>unknown request</source>
+        <translation>Demanda desconeguda</translation>
+    </message>
+</context>
+</TS>
