23f869c4f2befbece6b8856deecbad34865050f4
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-05-16 21:45:40 +0200

    Weblate commits (#90)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-openssh-askpass/fi/
    Translation: LXQt Configuration/lxqt-openssh-askpass
    
    Co-authored-by: Jouni Järvinen <jounijarvis@gmail.com>

---
+++ translations/lxqt-openssh-askpass_fi.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>OpenSSH-tunnistussalasanapyyntö</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="20"/>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>Syötä SSH-salasanasi:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="52"/>
+        <source>unknown request</source>
+        <translation>tuntematon pyyntö</translation>
+    </message>
+</context>
+</TS>
