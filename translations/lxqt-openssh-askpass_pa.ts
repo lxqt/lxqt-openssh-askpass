474b0cb8aafe6f73a08f4597ab79ca2fd3b8b243
A S Alam <amanpreet.alam@gmail.com>
2023-12-25 16:23:01 +0100

    Weblate commits
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-openssh-askpass/pa/
    Translation: LXQt Configuration/lxqt-openssh-askpass

---
+++ translations/lxqt-openssh-askpass_pa.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pa">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>OpenSSH ਪਰਮਾਣਿਕਤਾ ਵਕਤ ਬੇਨਤੀ</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="20"/>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>ਬੇਨਤੀ ਲਈ ਆਪਣਾ SSH ਵਾਕ ਦਿਓ:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="52"/>
+        <source>unknown request</source>
+        <translation>ਅਣਪਛਾਤੀ ਬੇਨਤੀ</translation>
+    </message>
+</context>
+</TS>
