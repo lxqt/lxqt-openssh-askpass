853a49af05c8c2093ddb088b6d36d2b0d9568507
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-02-11 09:16:04 +0100

    Weblate commits (#101)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-openssh-askpass/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-openssh-askpass/zh_TW/
    Translation: LXQt Configuration/lxqt-openssh-askpass
    
    Co-authored-by: BigELK176 ≡ <BigELK176@gmail.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>

---
+++ translations/lxqt-openssh-askpass_zh_TW.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>OpenSSH Authentication Passphrase request</source>
+        <translation>OpenSSH 身份驗證密碼請求</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="20"/>
+        <source>Enter your SSH passphrase for request:</source>
+        <translation>輸入您的 SSH 密碼：</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="52"/>
+        <source>unknown request</source>
+        <translation>不明要求</translation>
+    </message>
+</context>
+</TS>
